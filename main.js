fb2a2be75e6a3959aaa4991705debffeb62fc594
BangJunha <tkdlqjqkd@gmail.com>
2024-02-27 13:25:59 +0900

    Feat: add reactive web

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-// js
+const moreBtn = document.querySelector('.info .metadata .moreBtn');
+const title = document.querySelector('.info .metadata .title');
+
+moreBtn.addEventListener('click', () => {
+    moreBtn.classList.toggle('clicked');
+    title.classList.toggle('clamp');
+});
